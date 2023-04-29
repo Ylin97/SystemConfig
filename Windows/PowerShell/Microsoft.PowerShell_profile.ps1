fb4ad8ef8b4f04c0c936a27736cc47008dedf719
Ylin <yalin20210411@outlook.com>
2023-04-29 21:39:50 +0800

    fixed font's color for autosuggestion.

--- Windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,9 +2,12 @@ Import-Module DirColors
 #Import-Module posh-git 
 Import-Module oh-my-posh
 #Import-Module git-aliases
-#Set-PoshPrompt -Theme alien
-#Set-PoshPrompt -Theme material
-#Set-PoshPrompt -Theme spaceship
-Set-PoshPrompt -Theme amro
-#Set-PoshPrompt -Theme half-life
 
+# 设置 Terminal theme
+#Set-PoshPrompt -Theme atomicBit
+Set-PoshPrompt -Theme myspace
+#Set-PoshPrompt -Theme myspaceship
+#Set-PoshPrompt -Theme myhunk
+
+# 设置 autosuggestion（PredictionSource）字体颜色
+Set-PSReadLineOption -Colors @{ InlinePrediction = "DarkGray" }
